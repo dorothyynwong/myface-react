ef4f5c03c95a839314d79fa7dd6d3930edc10fb0
Dorothy Wong <dorothyynwong@gmail.com>
2024-07-31 09:46:30 +0100

    reformat code

--- frontend/src/users/UserDetails.tsx
+++ frontend/src/users/UserDetails.tsx
@@ -1,5 +1,5 @@
 // import {FetchUser} from './../data/fetchUserDetails.tsx';
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./UserDetails.scss"
 import "./../../public/styles.scss"
 import { useParams } from 'react-router-dom';
@@ -7,7 +7,7 @@ import moment from 'moment';
 import fetchData, { DataType, UserModel } from './../utils/fetchDataUtils.ts';
 
 const UserDetails: React.FC = () => {
-    const {userId} = useParams<{userId: string}> ();
+    const { userId } = useParams<{ userId: string }>();
     // const { user, isLoading, error } = FetchUser(Number(userId));
     const userUrl = `http://localhost:3001/users/${userId?.toString()}`;
     const [user, setUser] = useState<UserModel | null>(null);
@@ -17,19 +17,19 @@ const UserDetails: React.FC = () => {
     useEffect(() => {
         const fetchUserDetails = async () => {
             try {
-              const userData = await fetchData(userUrl, DataType.UserModel) as UserModel;
-              setUser(userData);
+                const userData = await fetchData(userUrl, DataType.UserModel) as UserModel;
+                setUser(userData);
             } catch (error: unknown) {
                 if (error instanceof Error) {
                     setError(error);
                     setIsLoading(false);
                 }
             }
-          };
-        
+        };
+
         fetchUserDetails();
         setIsLoading(false);
-    },[]);
+    }, []);
 
     // fetchData(userUrl, DataType.UserModel);
 
@@ -53,9 +53,9 @@ const UserDetails: React.FC = () => {
         <div className='userDetails'>
             <div className="profileSession">
                 <div className="imageParent">
-                    <img className="coverImage" src={user?.coverImageUrl} alt="cover image"/>
+                    <img className="coverImage" src={user?.coverImageUrl} alt="cover image" />
                     <div className="profilePad"></div>
-                    <img className="profileImage" src={user?.profileImageUrl} alt="profile image"/>
+                    <img className="profileImage" src={user?.profileImageUrl} alt="profile image" />
                     <div className="name">{user?.name}</div>
                     <div className="username">{user?.username}</div>
                     <div className="email">{user?.email}</div>
@@ -66,10 +66,10 @@ const UserDetails: React.FC = () => {
                 <div className="userPosts">
                     {user?.posts.map(post => (
                         <div className="userSinglePost" key={post.id}>
-                            <img src={post.imageUrl} alt={post.id.toString()}/>
+                            <img src={post.imageUrl} alt={post.id.toString()} />
                             <div className="childText">
                                 <div className="postedBy">{user?.username}</div>
-                                <div className="createdAt">{moment(post.createdAt).format("YYYY-MM-DD")}</div> 
+                                <div className="createdAt">{moment(post.createdAt).format("YYYY-MM-DD")}</div>
                                 <p className="message">{post.message}</p>
                             </div>
                         </div>
--- frontend/src/utils/fetchDataUtils.ts
+++ frontend/src/utils/fetchDataUtils.ts
@@ -1,56 +1,56 @@
 interface PostUserModel {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
 }
 export interface PostModel {
-    id: number;
-    message: string;
-    imageUrl: string;
-    createdAt: moment.Moment;
-    postedBy: PostUserModel;
-    likedBy: PostUserModel[];
-    dislikedBy: PostUserModel[];
+  id: number;
+  message: string;
+  imageUrl: string;
+  createdAt: moment.Moment;
+  postedBy: PostUserModel;
+  likedBy: PostUserModel[];
+  dislikedBy: PostUserModel[];
 }
 
 interface UserPostModel {
-    id: number;
-    message: string;
-    imageUrl: string;
-    createdAt: moment.Moment;
+  id: number;
+  message: string;
+  imageUrl: string;
+  createdAt: moment.Moment;
 }
 
 export interface UserModel {
-    id: number;
-    name: string;
-    username: string;
-    profileImageUrl: string;
-    coverImageUrl: string;
-    email: string;
-    posts: UserPostModel[];
-    likes: UserPostModel[];
-    dislikes: UserPostModel[];
+  id: number;
+  name: string;
+  username: string;
+  profileImageUrl: string;
+  coverImageUrl: string;
+  email: string;
+  posts: UserPostModel[];
+  likes: UserPostModel[];
+  dislikes: UserPostModel[];
 }
 
 export enum DataType {
-    Posts,
-    UserModel
-  }
-export default async function fetchData(url:string, type:  DataType): Promise<PostModel[] | UserModel> {
-    try {
-        const response = await fetch(url);
-        
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
+  Posts,
+  UserModel
+}
+export default async function fetchData(url: string, type: DataType): Promise<PostModel[] | UserModel> {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
 
     const data = await response.json();
-    
+
     if (type === DataType.UserModel) {
       return data as UserModel
     } else if (type === DataType.Posts) {
-        return data.results as PostModel[];
+      return data.results as PostModel[];
     } else {
       throw new Error("Unknown data type");
     }
