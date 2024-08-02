d268883a9db82b00e178adf1caba3f50bc0c107c
dorothywyn <dorothywyn@users.noreply.github.com>
2024-08-02 09:16:11 +0100

    create UserProfile and UserPosts, moved user profile session from UserProfile to UserDetails, pending to move user posts too

--- frontend/src/users/UserDetails.tsx
+++ frontend/src/users/UserDetails.tsx
@@ -11,8 +11,19 @@ const UserDetails: React.FC = () => {
 
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
+ 
+    const userProfileProps: UserProfileProps = {
+        name: user? user.name : "",
+        username: user? user.username : "",
+        profileImageUrl: user? user.profileImageUrl : "",
+        coverImageUrl: user? user.coverImageUrl : "",
+        email: user? user.email : ""
+    }
+
+        
 
     const splitName = user?.name.split(' ')[0];
+
 
     return (
         <div className='userDetails'>
---
+++ frontend/src/users/UserProfile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./UserDetails.scss"
+import "./../../public/styles.scss"
+import { UserProfileProps } from "./UserDetails.tsx";
+
+const UserProfile: React.FC<UserProfileProps> = (props: UserProfileProps) => {
+    return (
+        <div className="profileSession">
+            <div className="imageParent">
+                <img className="coverImage" src={props.coverImageUrl} alt="cover image" />
+                <div className="profilePad"></div>
+                <img className="profileImage" src={props.profileImageUrl} alt="profile image" />
+                <div className="name">{props.name}</div>
+                <div className="username">{props.username}</div>
+                <div className="email">{props.email}</div>
+            </div>
+        </div>
+    );
+
+};
+
+export default UserProfile;
